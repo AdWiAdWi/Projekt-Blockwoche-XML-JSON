7170587498f864a381d3dd6c0bf70c19b5a2426b
Frederico Fischer <frederico.fischer@stud.hslu.ch>
2020-02-15 10:13:21 +0100

    changes on schema and xml

---
+++ reservation.php
@@ -0,0 +1,7 @@
+<?php
+
+addReservation($_POST["vorname"], $_POST["nachname"], $_POST["geschlecht"], $_POST["adresse"], $_POST["stadt"], $_POST["telefonnummer"], $_POST["geburtstag"], $_POST["behinderungen"], $_POST["einzelzimmer"], $_POST["spezielles"], $_POST["event"]);
+
+function addReservation($vorname, $nachname, $geschlecht, $adresse, $stadt, $telefonnummer, $geburtstag, $behinderungen, $einzelzimmer, $spezielles, $event){
+
+}
