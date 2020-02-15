51f2a04b0fa2290f0606033e3140e5991b6c0a8b
Adrian Willi <49481738+AdWiAdWi@users.noreply.github.com>
2020-02-15 20:43:45 +0100

    nico needed help

--- index.php
+++ index.php
@@ -2,23 +2,21 @@
 include("parser.php");
 function main()
 {
-    // parse GET & POST params
-    $paramsXML = parse();
-
-    // validate params
-    $xsd = "get.xsd";
-    $result = $paramsXML->schemaValidate($xsd);
-    echo "validation with schema:";
-
     
+   // load XML
+    $data = file_get_contents('Datenbank.xml');
+    $xml = new DOMDocument();
+    $xml->loadXML($data);
+
     // create xhtml doc
     // this process should be used for every html we show to the user!
     $xsl = new DOMDocument();
     $xsl->load('transform.xsl');
     $processor = new XSLTProcessor();
     $processor->importStylesheet($xsl);
-    $dom = $processor->transformToDoc($paramsXML);
+    $dom = $processor->transformToDoc($xml);
     echo $dom->saveXML();
 }
 main();
-?>
+
+
