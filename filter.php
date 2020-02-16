f0c3fb9f5add379b4eed738ee2631bc5bfb54515
nicinho <iselinico.ch@gmail.com>
2020-02-16 19:45:17 +0100

    Changed filter.php

--- filter.php
+++ filter.php
@@ -1,9 +1,21 @@
 <?php
-    $xml = new DOMDocument();
 
-    $xsl = new DOMDocument();
-    $xsl->load('filter.xsl');
-    $processor = new XSLTProcessor();
-    $processor->importStylesheet($xsl);
-    $dom = $processor->transformToDoc($xml);
-    echo $dom->saveXML();
+loadFilter();
+
+function loadFilter() {
+       // load XML
+       $data = file_get_contents('Datenbank.xml');
+       $xml = new DOMDocument();
+       $xml->loadXML($data);
+   
+       // create xhtml doc
+       // this process should be used for every html we show to the user!
+       $xsl = new DOMDocument();
+       $xsl->load('filter.xsl');
+       $processor = new XSLTProcessor();
+       $processor->importStylesheet($xsl);
+       $dom = $processor->transformToDoc($xml);
+       echo $dom->saveXML();
+}
+
+?>
